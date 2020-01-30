2519e2a47bdac6c5989fe0b751f13487bb28902b
Silas Santiago <santiagosilas@outlook.com>
2020-01-30 13:35:15 -0300

    vue-tags-input

--- 2020-01-30/frontend/src/router/index.js
+++ 2020-01-30/frontend/src/router/index.js
@@ -6,35 +6,40 @@ import About from "@/views/about/Index";
 import Projects from "@/views/projects/Index";
 import ProjectDetails from "@/views/projects/Details";
 import Images from "@/views/samples/Images"
+import VueTagsInput from "@/views/samples/VueTagsInput"
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "About",
-      component: About
-    },   
-    {
-      path: "/projects",
-      name: "Projects",
-      component: Projects
-    },
-    {
-      path: "/projects/details/:id",
-      name: "ProjectDetails",
-      component: ProjectDetails
-    },
-    {
-      path: "/samples/images",
-      name: "Images",
-      component: Images
-    },         
-  ]
+    routes: [{
+            path: "/",
+            name: "Home",
+            component: Home
+        },
+        {
+            path: "/about",
+            name: "About",
+            component: About
+        },
+        {
+            path: "/projects",
+            name: "Projects",
+            component: Projects
+        },
+        {
+            path: "/projects/details/:id",
+            name: "ProjectDetails",
+            component: ProjectDetails
+        },
+        {
+            path: "/samples/images",
+            name: "Images",
+            component: Images
+        },
+        {
+            path: "/samples/tags",
+            name: "VueTagsInput",
+            component: VueTagsInput
+        },
+    ]
 })
---
+++ 2020-01-30/frontend/src/views/samples/VueTagsInput.vue
@@ -0,0 +1,55 @@
+<template>
+  <SimpleLayoutPage>
+    <div class="container">
+        <h6>Vue Tags Input</h6>
+
+        <vue-tags-input
+        v-model="tag"
+        :tags="tags"
+        :autocomplete-items="filteredItems"
+        @tags-changed="newTags => tags = newTags"
+        />
+
+    </div>
+  </SimpleLayoutPage>
+</template>
+
+<script>
+import SimpleLayoutPage from "@/components/layouts/SimpleLayout";
+import VueTagsInput from '@johmun/vue-tags-input';
+
+export default {
+  components: {
+    SimpleLayoutPage, VueTagsInput
+  },
+  data() {
+    return {
+      tag: '',
+      tags: [],
+      autocompleteItems: [{
+        text: 'Spain',
+      }, {
+        text: 'France',
+      }, {
+        text: 'USA',
+      }, {
+        text: 'Germany',
+      }, {
+        text: 'China',
+      }],
+    };
+  },
+  methods: {
+    AboutMe: function() {
+      this.$router.push("/about");
+    }
+  }
+};
+</script>
+
+<style scoped>
+div.background-image {
+  height: 200px;
+  background-repeat: no-repeat;
+}
+</style>
