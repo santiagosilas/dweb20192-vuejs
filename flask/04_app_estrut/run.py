83a2df1e5ee4ee9e18b6c93f1c742f82fefae020
Silas Santiago <santiagosilas@outlook.com>
2020-02-14 13:50:05 -0300

    Exemplos em Flask

---
+++ flask/04_app_estrut/run.py
@@ -0,0 +1,19 @@
+﻿# exemplo
+from app import app
+import signal, sys
+
+def handler(signal, frame):
+    print('Tchaul!')
+    sys.exit(0)
+signal.signal(signal.SIGINT, handler)
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        print('print this')
+    finally:
+        print('finally')
